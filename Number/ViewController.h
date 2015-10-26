7be494244379f84b3adf01d22fcf3b9e755ab5d2
ios <ios@Mac-mini-de-MBDS-9.local>
2015-10-26 09:31:39 +0100

    feat

---
+++ Number/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Number
+//
+//  Created by ios on 26/10/2015.
+//  Copyright (c) 2015 ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Number/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Number
+//
+//  Created by ios on 26/10/2015.
+//  Copyright (c) 2015 ios. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
